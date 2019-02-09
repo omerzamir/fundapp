bea0105c68da13abbfd560dff80f823653377f2a
Omer Zamir <zamir98@gmail.com>
2019-02-09 20:42:27 +0200

    Server with benchmark
    Signed-off-by: Omer Zamir <zamir98@gmail.com>

--- fund_test.go
+++ fund_test.go
@@ -1,17 +1,48 @@
 package funding
 
-import "testing"
+import (
+    "sync"
+    "testing"
+)
 
-func BenchmarkFund(b *testing.B) {
-    // Add as many dollars as we have iterations this run
-    fund := NewFund(b.N)
+const WORKERS = 10
 
-    // Burn through them one at a time until they are all gone
-    for i := 0; i < b.N; i++ {
-        fund.Withdraw(1)
+func BenchmarkWithdrawals(b *testing.B) {
+    // Skip N = 1
+    if b.N < WORKERS {
+        return
     }
 
-    if fund.Balance() != 0 {
-        b.Error("Balance wasn't zero:", fund.Balance())
+    // Add as many dollars as we have iterations this run
+    server := NewFundServer(b.N)
+
+    // Casually assume b.N divides cleanly
+    dollarsPerFounder := b.N / WORKERS
+
+    // WaitGroup structs don't need to be initialized
+    // (their "zero value" is ready to use).
+    // So, we just declare one and then use it.
+    var wg sync.WaitGroup
+
+    // Spawn off the workers
+    for i := 0; i < WORKERS; i++ {
+        wg.Add(1)
+        go func() {
+            defer wg.Done()
+            for i := 0; i < dollarsPerFounder; i++ {
+                server.Commands <- WithdrawCommand{ Amount: 1 }
+            }
+        }()
+    }
+
+	// Wait for all the workers to finish
+	wg.Wait()
+
+	balanceResponseChan := make(chan int)
+    server.Commands <- BalanceCommand{ Response: balanceResponseChan }
+	balance := <- balanceResponseChan
+	
+    if balance != 0 {
+        b.Error("Balance wasn't zero:", balance)
     }
 }
