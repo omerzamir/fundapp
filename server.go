8cdd66d94db4a9be103a31c03991e2d2415a745e
Omer Zamir <zamir98@gmail.com>
2019-02-09 21:59:34 +0200

    Finally - loop call was accidently deleted
    Signed-off-by: Omer Zamir <zamir98@gmail.com>

--- server.go
+++ server.go
@@ -6,59 +6,60 @@ import (
 
 // FundServer is a server of Fund.
 type FundServer struct {
-    commands chan interface{}
-    fund Fund
+	commands chan interface{}
+	fund     Fund
 }
 
 type withdrawCommand struct {
-    Amount int
+	Amount int
 }
 
 type balanceCommand struct {
-    Response chan int
+	Response chan int
 }
 
 // Balance returnes the balance of the server's fund.
 func (s *FundServer) Balance() int {
-    responseChan := make(chan int)
-    s.commands <- balanceCommand{ Response: responseChan }
-    return <- responseChan
+	responseChan := make(chan int)
+	s.commands <- balanceCommand{Response: responseChan}
+	return <-responseChan
 }
 
-// Withdraw is decreasing amount from the balance of the server. 
+// Withdraw is decreasing amount from the balance of the server.
 func (s *FundServer) Withdraw(amount int) {
-    s.commands <- withdrawCommand{ Amount: amount }
+	s.commands <- withdrawCommand{Amount: amount}
 }
 
 // NewFundServer returning Fund Server
 func NewFundServer(initialBalance int) *FundServer {
-    server := &FundServer{
-        // make() creates builtins like channels, maps, and slices
-        commands: make(chan interface{}),
-        fund: *NewFund(initialBalance),
-    }
-
-    return server
+	server := &FundServer{
+		// make() creates builtins like channels, maps, and slices
+		commands: make(chan interface{}),
+		fund:     *NewFund(initialBalance),
+	}
+	// Spawn off the server's main loop immediately
+	go server.loop()
+	return server
 }
 
 func (s *FundServer) loop() {
-    for command := range s.commands {
+	for command := range s.commands {
 
-        // command is just an interface{}, but we can check its real type
-        switch command.(type) {
+		// command is just an interface{}, but we can check its real type
+		switch command.(type) {
 
-        case withdrawCommand:
-            // And then use a "type assertion" to convert it
-            withdrawal := command.(withdrawCommand)
-            s.fund.Withdraw(withdrawal.Amount)
+		case withdrawCommand:
+			// And then use a "type assertion" to convert it
+			withdrawal := command.(withdrawCommand)
+			s.fund.Withdraw(withdrawal.Amount)
 
-        case balanceCommand:
-            getBalance := command.(balanceCommand)
-            balance := s.fund.Balance()
-            getBalance.Response <- balance
+		case balanceCommand:
+			getBalance := command.(balanceCommand)
+			balance := s.fund.Balance()
+			getBalance.Response <- balance
 
-        default:
-            panic(fmt.Sprintf("Unrecognized command: %v", command))
-        }
-    }
+		default:
+			panic(fmt.Sprintf("Unrecognized command: %v", command))
+		}
+	}
 }
